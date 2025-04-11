f8f473c1aa71d275fa7495aa1c7caf433c8a9c9f
SauravYog <sauravyogchennai@gmail.com>
2025-04-11 12:52:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/sauras-yoga/',  // <<< Add this line
+  base: '/',  // <<< Add this line
   
   server: {
     host: "::",
